b9e0b552a4c3a05ed79787c0bc2aa608c215c2cb
tomatoM4to <tomatomato2121@gmail.com>
2024-10-23 00:09:43 +0900

    Add book design link components on the main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,59 @@
-import Link from "next/link";
 import { mainPage } from "@/components/responsiveConfig";
 import { SideMenu } from "@/components/sideMenu/menu";
+import { Book } from "@/components/book";
+import { GiMeshNetwork } from "react-icons/gi";
+import { IoHardwareChipOutline } from "react-icons/io5";
+import { FaHeadSideVirus } from "react-icons/fa";
+import { GoDatabase } from "react-icons/go";
+import { post } from '@/components/responsiveConfig';
+
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
 
 function Dummy() {
     return <div className={`${mainPage}`}></div>;
 }
 
+function BookLayout({
+    children,
+    title
+}: {
+    children: React.ReactNode,
+    title: string
+}) {
+    return (
+        <div>
+            <h1 className="text-3xl mb-10">{title}</h1>
+            <div
+                className="
+                grid
+                grid-cols-1 sm:grid-cols-2 md:grid-cols-4
+                gap-20
+                place-items-center
+                custom-style
+                "
+            >
+                {children}
+            </div>
+        </div>
+    );
+}
+
 export default function Home() {
     return (
         <div className="mt-32 flex w-full">
-            <Dummy />
             <SideMenu />
-            <div className="flex-1 flex flex-col items-center">
-                <Link href="/os/1-Computer-System-Overview">os</Link>
-                <Link href="/network/1-Introduction">network</Link>
-                <Link href="/ai/1-AI-overview">ai</Link>
-                <Link href="/database/1-Introduction">database</Link>
-                <Link href="/oop/1-Introduction">OOP</Link>
+            <Dummy />
+            <div className="flex-1 flex justify-center">
+                <div className={`${post} ${inter.className} flex flex-col gap-52`}>
+                    <BookLayout title="컴퓨터 과학">
+                        <Book href="/os/1-Computer-System-Overview" icon={<IoHardwareChipOutline size={80} />} title="OS" />
+                        <Book href="/network/1-Introduction" icon={<GiMeshNetwork size={80} />} title="Network" />
+                        <Book href="/ai/1-AI-overview" icon={<FaHeadSideVirus size={80} />} title="AI" />
+                        <Book href="/database/1-Introduction" icon={<GoDatabase size={80} />} title="Database" />
+                    </BookLayout>
+                </div>
             </div>
         </div>
     );
