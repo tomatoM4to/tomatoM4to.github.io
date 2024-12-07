ba08d4deead427ea31e39dd39d4d3c7d148ddf7f
tomatoM4to <tomatomato2121@gmail.com>
2024-12-07 14:01:37 +0900

    Remove unused code

--- components/mdx.tsx
+++ components/mdx.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Image from 'next/image';
 
 export const code = {
     code: (props: React.HTMLAttributes<HTMLElement>) => {
--- components/miniTitle.tsx
+++
@@ -1,3 +0,0 @@
-export function MiniTitle({ title, className }: { title: string, className?: string }) {
-    return <p className={`text-sm text-gray-500 ${className}`}>{title}</p>
-}
