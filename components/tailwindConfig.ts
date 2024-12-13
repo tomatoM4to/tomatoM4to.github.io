d028dc582723b1152f0060342236afea4abaaf27
tomatoM4to <tomatomato2121@gmail.com>
2024-12-14 00:12:16 +0900

    small responsive fix

--- components/tailwindConfig.ts
+++ components/tailwindConfig.ts
@@ -1,7 +1,7 @@
 export class ResponsiveConfig {
     static sideList = `hidden lg:flex w-64 2xl:w-96`;
     static postPage = `lg:ml-64 2xl:ml-96`;
-    static post = `w-11/12 md:w-3/4 lg:w-2/3 2xl:w-1/2`;
+    static post = `w-11/12 lg:w-2/3 2xl:w-1/2`;
     static hamburger = `lg:hidden`;
     static hamburgerBtn = `w-8 h-8 md:w-10 md:h-10`
     static themeGridLayout = `w-11/12 grid-cols-1 md:grid-cols-2 xl:w-3/4 2xl:grid-cols-3 2xl:w-2/3`;
