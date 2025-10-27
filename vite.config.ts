2d6fe449e35692560d9b4ca8943ceb990eb071bf
tomatoM4to <tomatomato2121@gmail.com>
2025-10-27 19:47:30 +0900

    hotfix: devtoolsJson() 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import devtoolsJson from 'vite-plugin-devtools-json'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), devtoolsJson()],
 })
