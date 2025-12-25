55903e4f86ae7001db5a255061d54c6da542936d
tomatoM4to <tomatomato2121@gmail.com>
2025-12-25 21:13:41 +0900

    refactor: lazy-load page components

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Route, Routes } from 'react-router';
+import { lazy, Suspense } from 'react';
 import '@src/styles/App.css';
 import '@src/styles/Home.css';
 import '@src/styles/Nav.css';
@@ -11,25 +12,27 @@ import '@src/styles/Footer.css';
 import '@src/styles/License.css';
 import Nav from '@src/components/Nav';
 import Footer from '@src/components/Footer';
-import Home from '@src/pages/Home';
-import Post from '@src/pages/Post';
-import Search from '@src/pages/Search';
-import Tag from '@src/pages/Tag';
-import License from '@src/pages/License';
 import { MountProvider } from '@src/context/Mount';
 
+const Home = lazy(() => import('@src/pages/Home'));
+const Post = lazy(() => import('@src/pages/Post'));
+const Search = lazy(() => import('@src/pages/Search'));
+const Tag = lazy(() => import('@src/pages/Tag'));
+const License = lazy(() => import('@src/pages/License'));
 
 export default function App({ markdown }: { markdown: string }) {
   return (
     <MountProvider>
       <Nav />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/posts/:post" element={<Post markdown={markdown} />} />
-        <Route path='/search' element={<Search />} />
-        <Route path='/tags' element={<Tag />} />
-        <Route path='/license' element={<License />} />
-      </Routes>
+      <Suspense fallback={<></>}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/posts/:post" element={<Post markdown={markdown} />} />
+          <Route path='/search' element={<Search />} />
+          <Route path='/tags' element={<Tag />} />
+          <Route path='/license' element={<License />} />
+        </Routes>
+      </Suspense>
       <Footer />
     </MountProvider>
   )
