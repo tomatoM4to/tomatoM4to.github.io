92204cf41eccdaec8b608ad360abe80698e5ba46
tomatoM4to <tomatomato2121@gmail.com>
2026-02-14 23:47:40 +0900

    update favicon

--- shared/common.ts
+++ shared/common.ts
@@ -4,7 +4,7 @@ export const SITE_DOMAIN = 'https://tomatom4to.github.io';
 export const SITE_NAME = "tomatoM4to Tech Log";
 export const DEFAULT_DESC = '웹, 시스템 아키텍처, 그리고 컴퓨터 과학(CS) 원리를 깊이 있게 탐구합니다.';
 export const DEFAULT_KEYWORDS = '백엔드, 프론트 엔드, Nodejs, Backend, CS, 컴퓨터과학, 서버개발, 아키텍처';
-export const DEFAULT_IMAGE = 'https://tomatom4to.github.io/vite.svg';
+export const DEFAULT_IMAGE = 'https://tomatom4to.github.io/terminal.svg';
 export const POSTS_PER_PAGE = 5;
 
 export type TagsData = Record<string, number>;
